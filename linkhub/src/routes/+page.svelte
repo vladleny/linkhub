cc88a6f273d55922757aeefa9cc8799007e1cbb0
vladleny <vlad.provotar@gmail.com>
2023-02-25 19:31:47 +0200

    menu (temporary colors)

--- linkhub/src/routes/+page.svelte
+++ linkhub/src/routes/+page.svelte
@@ -1,5 +1,27 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<h1>Let's get cracking bones!</h1>
+<div class="wrapper w-full h-full">
+ <header class="w-full h-full">
+	<div  class="menu w-full h-24 flex justify-between items-center">
+		<div class="menu__logo">
+			<div class="logo__sq bg-[#141416] w-24 h-24 absolute top-0 left-0" ><div class="absolute top-6 left-5 text-2xl leading-5 font-bold">link<br>hub`</div>
+				<div class="logo__round bg-[#15171F] w-14 h-14 rounded-full flex justify-center items-center ml-16 mt-16">
+					<div class="logo__round bg-[#141416] w-12 h-12 rounded-full flex justify-center items-center ml-1 mt-1">
+						<div class="logo__plus flex justify-center items-center">
+							<div class="plus__el bg-white h-1 w-6 absolute rounded-full"></div>
+							<div class="plus__el bg-white h-6 w-1 absolute rounded-full"></div>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+			
+		</div>
+		<ul class="menu__row font-bold flex mr-24 items-center">
+			<li class="menu__link ">Sign Up</li>
+			<li class="menu__link mx-12">Login</li>
+			<li class="menu__link  bg-[#141416] h-14 w-14 flex justify-center items-center rounded-full">ENG</li>
+		</ul>
+	</div>
+ </header>
 </div>
