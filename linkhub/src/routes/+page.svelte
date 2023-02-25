a2ff871d447c27c999a3d23e1e2e4b1ced3e14ca
Vyacheslav <52282118+SlavikArt@users.noreply.github.com>
2023-02-25 17:46:20 +0200

    Hello World!

--- linkhub/src/routes/+page.svelte
+++ linkhub/src/routes/+page.svelte
@@ -1,5 +1,3 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
-
 <div class="container h-full mx-auto flex justify-center items-center">
-	<h1>Let's get cracking bones!</h1>
+	<h1>Hello World!</h1>
 </div>
