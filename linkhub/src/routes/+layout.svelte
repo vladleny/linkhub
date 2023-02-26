cefda66e6ab7807ed6f603976e0c3c2919b0884f
vladleny <vlad.provotar@gmail.com>
2023-02-26 03:29:02 +0200

    color fix + theme

--- linkhub/src/routes/+layout.svelte
+++ linkhub/src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import '@skeletonlabs/skeleton/themes/theme-crimson.css';
+	
 	import '@skeletonlabs/skeleton/styles/all.css';
 	import '../app.postcss';
 </script>
--- linkhub/src/routes/+page.svelte
+++ linkhub/src/routes/+page.svelte
@@ -1,12 +1,12 @@
 
 
-<div class="wrapper w-full h-full">
+<div class="wrapper w-full h-full bg-surface-500">
  <header class="w-full h-full">
 	<div  class="menu w-full h-24 flex justify-between items-center">
 		<div class="menu__logo">
-			<div class="logo__sq bg-[#141416] w-24 h-24 absolute top-0 left-0" ><div class="absolute top-6 left-5 text-2xl leading-5 font-bold">link<br>hub`</div>
-				<div class="logo__round bg-[#15171F] w-14 h-14 rounded-full flex justify-center items-center ml-16 mt-16">
-					<div class="logo__round bg-[#141416] w-12 h-12 rounded-full flex justify-center items-center ml-1 mt-1">
+			<div class="logo__sq bg-primary-500 w-24 h-24 absolute top-0 left-0" ><div class="absolute top-6 left-5 text-2xl leading-5 font-bold">link<br>hub`</div>
+				<div class="logo__round bg-surface-500 w-14 h-14 rounded-full flex justify-center items-center ml-16 mt-16">
+					<div class="logo__round bg-primary-500 w-12 h-12 rounded-full flex justify-center items-center ml-1 mt-1">
 						<div class="logo__plus flex justify-center items-center">
 							<div class="plus__el bg-white h-1 w-6 absolute rounded-full"></div>
 							<div class="plus__el bg-white h-6 w-1 absolute rounded-full"></div>
@@ -19,7 +19,7 @@
 		<ul class="menu__row font-bold flex mr-24 items-center">
 			<li class="menu__link ">Sign Up</li>
 			<li class="menu__link mx-12">Login</li>
-			<li class="menu__link  bg-[#141416] h-14 w-14 flex justify-center items-center rounded-full">ENG</li>
+			<li class="menu__link text-sm bg-primary-500 h-12 w-12 flex justify-center items-center rounded-full">ENG</li>
 		</ul>
 	</div>
  </header>
