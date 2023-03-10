20f404e5b8df2f14facab148063eebbece6275c7
vladleny <vlad.provotar@gmail.com>
2023-03-10 14:43:38 +0200

    add, edit, del funcs

--- linkhub/src/routes/+layout.svelte
+++ linkhub/src/routes/+layout.svelte
@@ -1,20 +1,31 @@
 <script>
+
+	
 	import '../theme.postcss';
 	import '@skeletonlabs/skeleton/styles/all.css';
 	import '../app.postcss';
+
+	import Modal from './+page.svelte'
+	let showModal = false;
+	
+	export const toggleModal = () => {
+		showModal = !showModal;
+	}
 </script>
+
+
 
 <header class="w-full top-0 bg-surface-500">
 	<nav class="flex flex-row items-center w-full mx-auto justify-between">
 		<div class="logo bg-primary-500 w-24 h-24 flex flex-none justify-center items-center">
 			<div class="logo-text flex top-6 left-5 text-2xl leading-5 font-bold items-center">link<br>hub`</div>
 			<div class="logo-plus bg-surface-500 w-14 h-14 rounded-full flex justify-center items-center absolute ml-20 mt-20">
-				<div class="bg-primary-500 w-12 h-12 rounded-full flex justify-center items-center ml-1 mt-1">
+				<button class="bg-primary-500 w-12 h-12 rounded-full flex justify-center items-center ml-1 mt-1 z-1" on:click={toggleModal}>
 					<div class="flex justify-center items-center">
 						<div class="bg-white h-1 w-6 absolute rounded-full"></div>
 						<div class="bg-white h-6 w-1 absolute rounded-full"></div>
 					</div>
-				</div>
+				</button>
 			</div>
 		</div>
 		<ul class="flex flex-nowrap p-2 mr-24 items-center flex-row justify-between w-1/5 font-bold">
@@ -31,7 +42,13 @@
 	</nav>
 </header>
 
+<Modal {showModal}></Modal>
+
+
 <slot />
+
+
+
 
 <!-- <footer>
 	bla bla bla
