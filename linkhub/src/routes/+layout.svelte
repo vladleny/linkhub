3f1d0a8caaf4327178e4fdc03d8717870a04e653
vladleny <vlad.provotar@gmail.com>
2023-03-11 16:08:40 +0200

    add btn fix

--- linkhub/src/routes/+layout.svelte
+++ linkhub/src/routes/+layout.svelte
@@ -6,41 +6,16 @@
 	import '../app.postcss';
 
 	import Modal from './+page.svelte'
+	
 	let showModal = false;
 	
-	export const toggleModal = () => {
-		showModal = !showModal;
-	}
+	
+	
+
 </script>
 
 
 
-<header class="w-full top-0 bg-surface-500">
-	<nav class="flex flex-row items-center w-full mx-auto justify-between">
-		<div class="logo bg-primary-500 w-24 h-24 flex flex-none justify-center items-center">
-			<div class="logo-text flex top-6 left-5 text-2xl leading-5 font-bold items-center">link<br>hub`</div>
-			<div class="logo-plus bg-surface-500 w-14 h-14 rounded-full flex justify-center items-center absolute ml-20 mt-20">
-				<button class="bg-primary-500 w-12 h-12 rounded-full flex justify-center items-center ml-1 mt-1 z-1" on:click={toggleModal}>
-					<div class="flex justify-center items-center">
-						<div class="bg-white h-1 w-6 absolute rounded-full"></div>
-						<div class="bg-white h-6 w-1 absolute rounded-full"></div>
-					</div>
-				</button>
-			</div>
-		</div>
-		<ul class="flex flex-nowrap p-2 mr-24 items-center flex-row justify-between w-1/5 font-bold">
-			<li>
-				Sign Up
-			</li>
-			<li>
-				Login
-			</li>
-			<li class="text-sm bg-primary-500 h-12 w-12 flex justify-center items-center rounded-full">
-				ENG
-			</li>
-		</ul>
-	</nav>
-</header>
 
 <Modal {showModal}></Modal>
 
--- linkhub/src/routes/+page.svelte
+++ linkhub/src/routes/+page.svelte
@@ -3,9 +3,10 @@
 	// @ts-nocheck
 		export let showModal = false;
 		
-		const toggleModal = () => {
+		let toggleModal = () => {
 			showModal = !showModal;
 		}
+		
 		
 		
 	
@@ -17,6 +18,7 @@
 		
 	
 		function addLink(){
+		showModal = !showModal;
 		 const newLink = {
 			  id: linkList.length + 1,
 			  name: nameInput,
@@ -24,7 +26,8 @@
 			  editing: false
 		 }
 		 linkList = [...linkList, newLink]; 
-		 showModal = !showModal;
+		 showModal = false;
+		 console.log(showModal)
 	}
 	
 		/**
@@ -34,6 +37,7 @@
 		function setEditing(i, isEditing){
 			// @ts-ignore
 			linkList[i].editing = isEditing;
+
 		}
 	
 	
@@ -50,6 +54,34 @@
 		
 		
 	</script>
+
+
+<header class="w-full top-0 bg-surface-500">
+	<nav class="flex flex-row items-center w-full mx-auto justify-between">
+		<div class="logo bg-primary-500 w-24 h-24 flex flex-none justify-center items-center">
+			<div class="logo-text flex top-6 left-5 text-2xl leading-5 font-bold items-center">link<br>hub`</div>
+			<div class="logo-plus bg-surface-500 w-14 h-14 rounded-full flex justify-center items-center absolute ml-20 mt-20">
+				<button class="bg-primary-500 w-12 h-12 rounded-full flex justify-center items-center ml-1 mt-1 z-1" on:click={toggleModal}>
+					<div class="flex justify-center items-center">
+						<div class="bg-white h-1 w-6 absolute rounded-full"></div>
+						<div class="bg-white h-6 w-1 absolute rounded-full"></div>
+					</div>
+				</button>
+			</div>
+		</div>
+		<ul class="flex flex-nowrap p-2 mr-24 items-center flex-row justify-between w-1/5 font-bold">
+			<li>
+				Sign Up
+			</li>
+			<li>
+				Login
+			</li>
+			<li class="text-sm bg-primary-500 h-12 w-12 flex justify-center items-center rounded-full">
+				ENG
+			</li>
+		</ul>
+	</nav>
+</header>
 	
 	{#if showModal}
 	<div class="blur absolute right-0 top-0 bg-primary-700 w-full h-full opacity-[80%] z-10"></div>
@@ -114,39 +146,4 @@
 	
 
 
-<!-- <div class="wrapper w-full h-full bg-surface-500">
-	<header class="w-full">
-		<div  class="menu w-full h-24 flex justify-between items-center">
-			<div class="menu__logo">
-				<div class="logo__sq bg-primary-500 w-24 h-24 absolute top-0 left-0" ><div class="absolute top-6 left-5 text-2xl leading-5 font-bold">link<br>hub`</div>
-					<div class="logo__round bg-surface-500 w-14 h-14 rounded-full flex justify-center items-center ml-16 mt-16">
-						<div class="logo__round bg-primary-500 w-12 h-12 rounded-full flex justify-center items-center ml-1 mt-1">
-							<div class="logo__plus flex justify-center items-center">
-								<div class="plus__el bg-white h-1 w-6 absolute rounded-full"></div>
-								<div class="plus__el bg-white h-6 w-1 absolute rounded-full"></div>
-							</div>
-						</div>
-					</div>
-				</div>
-				
-			</div>
-			<ul class="menu__row font-bold flex mr-24 items-center">
-				<li class="menu__link ">Sign Up</li>
-				<li class="menu__link mx-12">Login</li>
-				<li class="menu__link text-sm bg-primary-500 h-12 w-12 flex justify-center items-center rounded-full">ENG</li>
-			</ul>
-		</div>
-	</header>
-	<div class="page flex max-h-[100vh] justify-center items-center overflow-y-auto overflow-x-hidden md:mt-20 sm:mt-6">
-		<div class="page__links  grid grid-cols-1  2xl:grid-cols-6 gap-8 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-4 md:gap-[50px] sm:grid-cols-2  lg:max-h-[50%] md:max-h-[100%] sm:max-h-[100%]">
-			{#each arr as i }
-			<div class="page__link  bg-primary-500 h-24 w-48 rounded-3xl flex justify-center items-center">
-				<div class="page__link-container leading-none max-w-[200px] overflow-hidden mx-6">
-					<div class="page__name  font-bold text-md">name {i + 1}</div>
-					<div class="page__url font-semibold text-sm text-secondary-500 lowercase">url {i + 1}</div>
-				</div>
-			</div>
-			{/each}
-		</div>
-	</div>
-</div> -->
+
